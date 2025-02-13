df1500cb3b810cf21725c77d4de5e019c8c3c2d2
Satpal Jangir <satpaljangir22@gmail.com>
2025-02-13 11:26:31 +0530

    changed configs

--- page-fixtures/main-page.ts
+++ page-fixtures/main-page.ts
@@ -2,11 +2,12 @@ import { Page } from "@playwright/test";
 
 export class MainPage {
   protected page: Page;
-  private baseUrl = "https://www.lambdatest.com/selenium-playground/";
   constructor(page: Page) {
     this.page = page;
   }
   protected async GoToBaseUrl() {
-    await this.page.goto(this.baseUrl, { waitUntil: "domcontentloaded" });
+    await this.page.goto("/selenium-playground/", {
+      waitUntil: "domcontentloaded",
+    });
   }
 }
